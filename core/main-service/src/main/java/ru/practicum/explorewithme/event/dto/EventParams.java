9ec6457ec5c706bbaa2c2d693adcd4ed12d6b516
Vladislava Babukova <Vlada.409b@gmail.com>
2026-01-12 21:06:58 +0300

    Добавлены модули infra и core

---
+++ core/main-service/src/main/java/ru/practicum/explorewithme/event/dto/EventParams.java
@@ -0,0 +1,26 @@
+package ru.practicum.explorewithme.event.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.practicum.explorewithme.event.enums.EventsSort;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class EventParams {
+    String text;
+    List<Long> categories;
+    Boolean paid;
+    LocalDateTime rangeStart;
+    LocalDateTime rangeEnd;
+    Boolean onlyAvailable;
+    EventsSort eventsSort;
+    int from;
+    int size;
+}
