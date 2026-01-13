571bff7064547814ddfb036200cead2e7e1b60b8
Vladislava Babukova <Vlada.409b@gmail.com>
2026-01-13 22:25:01 +0300

    Добавлены infra и core

--- core/main-service/src/main/java/ru/practicum/explorewithme/ExploreWithMeMainService.java
+++ core/main-service/src/main/java/ru/practicum/explorewithme/ExploreWithMeMainService.java
@@ -2,9 +2,11 @@ package ru.practicum.explorewithme;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @ComponentScan(basePackages = {"ru.practicum.explorewithme", "ru.practicum.client"})
 public class ExploreWithMeMainService {
     public static void main(String[] args) {
--- stats/stats-server/src/main/java/ru/practicum/explorewithme/ExploreWithMeStatsServer.java
+++ stats/stats-server/src/main/java/ru/practicum/explorewithme/ExploreWithMeStatsServer.java
@@ -2,8 +2,10 @@ package ru.practicum.explorewithme;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ExploreWithMeStatsServer {
     public static void main(String[] args) {
         SpringApplication.run(ExploreWithMeStatsServer.class, args);
